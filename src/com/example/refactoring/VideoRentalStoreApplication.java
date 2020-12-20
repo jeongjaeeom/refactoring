739dfa01b15817b7fea23223642de897ab664c3b
jeongjaeeom <jeongjae.eom@gmail.com>
2020-12-20 22:49:10 +0900

    원래의 프로그램

--- src/com/example/refactoring/VideoRentalStoreApplication.java
+++ src/com/example/refactoring/VideoRentalStoreApplication.java
@@ -3,7 +3,19 @@ package com.example.refactoring;
 public class VideoRentalStoreApplication {
 
   public static void main(String[] args) {
+    Customer teddy = new Customer("teddy");
 
+    Movie thor = new Movie("Thor", Movie.REGULAR);
+    Movie blackPanther = new Movie("Black Panther", Movie.NEW_RELEASE);
+
+    Rental rentalThor = new Rental(thor, 3);
+    Rental rentalBlackPanther = new Rental(blackPanther, 2);
+
+    teddy.addRental(rentalThor);
+    teddy.addRental(rentalBlackPanther);
+
+    String result = teddy.statement();
+    System.out.println(result);
   }
 
 }
