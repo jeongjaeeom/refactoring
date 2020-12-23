b1e5e06106510e50583314de87608044ce6e9a2e
jeongjaeeom <jeongjae.eom@gmail.com>
2020-12-24 00:00:13 +0900

    임시변수 없애기 임시변수를 메서드 호출로 전환(Replace Temp with Query)

--- src/com/example/refactoring/Customer.java
+++ src/com/example/refactoring/Customer.java
@@ -21,23 +21,32 @@ public class Customer {
   }
 
   public String statement() {
-    double totalAmount = 0;
-    int frequentRenterPoints = 0;
     String result = getName() + " 고객님의 대여 기록\n";
 
     for (Rental each : rentals) {
-      // 경우에 따른 적립 포인트 지급 함수를 호출
-      frequentRenterPoints = each.getFrequentRenterPoints();
-
       // 이번에 대여하는 비디오 정보와 대여료를 출력
       result += "\t" + each.getMovie().getTitle() + "\t" + each.getCharge() + "\n";
-      // 현재까지 누적된 총 대여료
-      totalAmount += each.getCharge();
     }
 
     // 푸터 행 추가
-    result += "누적 대여료: " + totalAmount + "\n";
-    result += "적립 포인트: " + frequentRenterPoints + "\n";
+    result += "누적 대여료: " + getTotalCharge() + "\n";
+    result += "적립 포인트: " + getTotalFrequentRenterPoints() + "\n";
+    return result;
+  }
+
+  private double getTotalCharge() {
+    double result = 0;
+    for (Rental each : rentals) {
+      result += each.getCharge();
+    }
+    return result;
+  }
+
+  private int getTotalFrequentRenterPoints() {
+    int result = 0;
+    for (Rental each : rentals) {
+      result += each.getFrequentRenterPoints();
+    }
     return result;
   }
 
