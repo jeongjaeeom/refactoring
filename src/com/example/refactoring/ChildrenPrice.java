7b6046545e41419e66d8ff5ebd9172b0e377fa93
jeongjaeeom <jeongjae.eom@gmail.com>
2020-12-29 17:08:52 +0900

    조건문을 재정의로 전환(Replace Conditional with Polymorphism) - Move 클래스의 getCharge() 메소드를 전환.

--- src/com/example/refactoring/ChildrenPrice.java
+++ src/com/example/refactoring/ChildrenPrice.java
@@ -6,4 +6,13 @@ public class ChildrenPrice extends Price {
   int getPriceCode() {
     return Movie.CHILDREN;
   }
+
+  @Override
+  double getCharge(int daysRented) {
+    double result = 1.5;
+    if (daysRented > 3) {
+      result += (daysRented - 3) * 1.5;
+    }
+    return result;
+  }
 }
